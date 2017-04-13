4a8e6b9553ac3fe8bde070e2cae502fd4b124924
shenrm <shenrm@51guanhuai.cn>
2017-04-13 16:31:18 +0800

    formatting

--- src/main/java/com/agh/flume/ETLInterceptor.java
+++ src/main/java/com/agh/flume/ETLInterceptor.java
@@ -1,27 +1,23 @@
-//add begin
 package com.agh.flume;
-//add end
 
 import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
-import com.google.gson.JsonObject;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
-import org.apache.flume.event.SimpleEvent;
 import org.apache.flume.interceptor.Interceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
-public class ETLInterceptor implements Interceptor{
+public class ETLInterceptor implements Interceptor {
+
     private static Logger logger = LoggerFactory.getLogger(ETLInterceptor.class);
+
     @Override
     public void initialize() {
 
     }
+
     @Override
     public Event intercept(Event event) {
         String body = new String(event.getBody(), Charsets.UTF_8);
@@ -49,13 +45,11 @@ public class ETLInterceptor implements Interceptor{
 
     }
 
-    public static class Builder implements Interceptor.Builder{
+    public static class Builder implements Interceptor.Builder {
         //使用Builder初始化Interceptor
         @Override
         public Interceptor build() {
-	//add begin
             return new ETLInterceptor();
-	//add end
         }
 
         @Override
